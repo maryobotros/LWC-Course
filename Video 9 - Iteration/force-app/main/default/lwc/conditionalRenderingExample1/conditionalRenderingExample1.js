1343b843ae7f31aaf17a67eb7b24e018b10d5d5f
Maryo Botros <maryo.botros@farmersinsurance.com>
2022-07-12 22:45:57 -0700

    Iteration

---
+++ Video 9 - Iteration/force-app/main/default/lwc/conditionalRenderingExample1/conditionalRenderingExample1.js
@@ -0,0 +1,5 @@
+import { LightningElement, track } from 'lwc';
+
+export default class ConditionalRenderingExample1 extends LightningElement {
+    @track cityList = ["Los Angeles", "New York", "Seattle"];
+}
